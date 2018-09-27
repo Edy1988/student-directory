e403700ec8ce5e6d1b5b425b2ecdf294d1259234
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-09-27 18:05:59 +0100

    Step8, exercise1 - added adjusted methood to print names with numbers using to.enum_with_index(1) method.

--- directory.rb
+++ directory.rb
@@ -19,16 +19,14 @@ def print_header
   puts "------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+  students.to_enum.with_index(1).each {|value, index| puts "#{index}. #{value[:name]}"}
 end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice" 
+  puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
   while !name.empty? do
