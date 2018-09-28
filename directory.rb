50a4fc753c1607a56e6de832477540cb25a7e8e5
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-09-28 21:14:20 +0100

    Added .gitignore and students.csv

--- directory.rb
+++ directory.rb
@@ -5,6 +5,7 @@ def interactive_menu
   loop do
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit"
 
     selection = gets.chomp
@@ -16,6 +17,8 @@ def interactive_menu
       print_header
       print(students)
       print_footer(students)
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -50,7 +53,21 @@ def input_students
   students
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+
 students = input_students
 print_header
 print(students)
 print_footer(students)
+save_students
