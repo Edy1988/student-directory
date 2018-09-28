2921f0836197c720813c4a5c58a0ef842d8cba48
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-09-28 13:03:48 +0100

    Small change

--- directory.rb
+++ directory.rb
@@ -1,17 +1,4 @@
-# start with a list of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+
 # add interactive menu
 def interactive_menu
   students = []
