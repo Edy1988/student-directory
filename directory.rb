ddb4d2a3c7353cda4bd81188de8ab809a0d9c8d3
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-09-29 18:43:30 +0100

    Use File class convenience methods so we dont need to open/close the file streams

--- directory.rb
+++ directory.rb
@@ -128,16 +128,15 @@ def input_filename
 end
 
 def save_students
-  # open the file for writing
   filename = input_filename
-  file = File.open(filename, "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+
+  File.open(filename, "w") do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
 
   if @students.count == 1
     puts "Saved 1 student"
@@ -157,12 +156,10 @@ def load_students
 end
 
 def load_students_from(filename)
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
+  File.readlines(filename).each do |line|
     name, cohort = line.chomp.split(',')
     add_student(name, cohort.to_sym)
   end
-  file.close
 
   if @students.count == 1
     puts "Loaded 1 student from #{filename}"
